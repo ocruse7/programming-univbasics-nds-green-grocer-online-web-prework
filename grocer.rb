5b67319088a9c9ade9534f135433917bd0dd0b08
Owen Cruse <ocruse7@gmail.com>
2019-10-25 19:04:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,17 +1,25 @@
 def find_item_by_name_in_collection(name, collection)
+  
   collection.each do |item_info|
+    
     if item_info[:item] == name
       return item_info
-    end #if
-  end #each
+      
+    end
+  end
+  
   nil
-end #method
+  
+end
 
 def consolidate_cart(cart)
+  
   new_cart = []
+  
   cart.each do |cart_item|
     item_name = cart_item[:item]
     item_info = find_item_by_name_in_collection(item_name, new_cart)
+    
     if item_info
       item_info[:count] += 1
     else
@@ -21,18 +29,23 @@ def consolidate_cart(cart)
         :clearance => cart_item[:clearance],
         :count => 1
       }
-    end #if
-  end #each
+    end
+  end
+  
   new_cart
-end #method consolidate_cart
+  
+end
 
 def apply_coupons(cart, coupons)
+  
   coupons.each do |coupon|
     item_info = find_item_by_name_in_collection(coupon[:item], cart)
 	  item_w_coupon = find_item_by_name_in_collection(coupon[:item]+" W/COUPON", cart)
+	  
     if item_w_coupon and item_info[:count] >= coupon[:num]
 	    item_w_coupon[:count] += coupon[:num]
 	    item_info[:count] -= coupon[:num]
+	    
 	  elsif item_info and item_info[:count] >= coupon[:num]
       cart << {
         :item => coupon[:item] + " W/COUPON",
@@ -40,21 +53,28 @@ def apply_coupons(cart, coupons)
         :clearance => item_info[:clearance],
         :count => coupon[:num]
       }
+      
       item_info[:count] -= coupon[:num]
-    end #if
-  end #each
-  #cart.delete_if{|item_info| item_info[:count] <= 0}
+    end 
+  end 
+  
   cart
-end #method apply_coupons
+  
+end
 
 def apply_clearance(cart)
+  
   cart.each do |item_info|
+    
     if item_info[:clearance]
       item_info[:price] *= 0.8
-    end #if
-  end #each
+      
+    end 
+  end 
+  
   cart
-end #method
+  
+end
 
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
@@ -66,8 +86,9 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-  consol_cart = consolidate_cart(cart)
-  cart_w_coupons_applied = apply_coupons(consol_cart, coupons)
+  
+  consolitated_cart = consolidate_cart(cart)
+  cart_w_coupons_applied = apply_coupons(consolitated_cart, coupons)
   final_cart = apply_clearance(cart_w_coupons_applied)
   
   total = 0
@@ -77,6 +98,8 @@ def checkout(cart, coupons)
   
   if total > 100
     total *= 0.9
-  end #if
+  end
+  
   return total.round(2)
+  
 end
